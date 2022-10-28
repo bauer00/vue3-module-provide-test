ee077355f6f3a65cd6069d6487bfaf15b6feac18
Cyrill Steinmetz <cyrill.steinmetz@certifaction.io>
2022-10-28 13:52:00 +0200

    Use inject from module in local component

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { inject } from "vue";
 import { provideTestKey } from "../plugins/test_plugin";
+import { moduleProvideTestKey } from "@test/module";
 
 const testInject = inject(provideTestKey);
+const moduleTestInject = inject(moduleProvideTestKey);
 </script>
 
 <template>
     <div class="test-component">
         <h2>Local Test Component</h2>
         <div>Local inject: {{ testInject }}</div>
+        <div>Module inject: {{ moduleTestInject }}</div>
     </div>
 </template>
